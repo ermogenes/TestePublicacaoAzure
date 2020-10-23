f04169a796712cd27c4feeb747148ee978cdffdc
Ermogenes Palacio <ermogenes@gmail.com>
2020-10-23 19:17:49 -0300

    Teste de conexão com MySQL in-app

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using TestePublicacaoAzure.db;
 
 namespace TestePublicacaoAzure.Controllers
 {
@@ -26,12 +27,22 @@ namespace TestePublicacaoAzure.Controllers
         [HttpGet]
         public IEnumerable<WeatherForecast> Get()
         {
+            var ingredientes = new List<string>();
+            using (var db = new hamburgueriaContext())
+            {
+                var ings = db.Ingrediente.ToList();
+                foreach(var i in ings)
+                {
+                    ingredientes.Add(i.Nome);
+                }
+            }
+
             var rng = new Random();
             return Enumerable.Range(1, 5).Select(index => new WeatherForecast
             {
                 Date = DateTime.Now.AddDays(index),
                 TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
+                Summary = ingredientes[rng.Next(ingredientes.Count())]
             })
             .ToArray();
         }
